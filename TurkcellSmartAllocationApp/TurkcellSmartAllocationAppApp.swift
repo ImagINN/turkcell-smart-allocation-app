d589245607cd78dc6eaeeae4f3f3d0034c60cc62
ImagINN <muhammed.gokhan55@gmail.com>
2026-01-14 21:38:52 +0300

    Initial Commit

---
+++ TurkcellSmartAllocationApp/TurkcellSmartAllocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurkcellSmartAllocationAppApp.swift
+//  TurkcellSmartAllocationApp
+//
+//  Created by Gokhan on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TurkcellSmartAllocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
